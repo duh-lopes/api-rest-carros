cd2671baf2d627458b2ca907eb019db1e83b8996
duh-lopes <comercialeduardolopes@gmail.com>
2023-03-27 16:21:22 -0300

    first

---
+++ src/main/java/com/garagem/boot/Carro.java
@@ -0,0 +1,26 @@
+package com.garagem.boot;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Carro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	Long id;
+	String marca;
+	String modelo;
+	Integer ano;
+	Integer preco;
+
+}
---
+++ src/test/java/com/garagem/boot/GaragemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garagem.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
